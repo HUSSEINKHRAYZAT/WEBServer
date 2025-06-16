5e2814692d0a8a17986ed2078da3eadc7cc6bb2c
Hussein Khrayzat <hkhrayza@c2r6s6.42beirut.com>
2025-06-16 21:22:07 +0300

    dddd

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,15 @@
 #include "headers.hpp"
 
 int main(int argc, char **argv) {
-    std::string config = (argc > 1) ? argv[1] : "default.conf";
+    // if (argc != 2) {
+    //     std::cerr << "Usage: " << argv[0] << " [configuration file]" << std::endl;
+    //     return 1;
+    // }
+
+    std::string config = (argc == 1) ? "default.conf" :  argv[1];
 
     try {
+        std::cout << "Launching server with config: " << config << std::endl;
         Server server(config);
         server.run();
     } catch (const std::exception &e) {
